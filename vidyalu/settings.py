63074971f3da0cfddd7de4af0cfae00460a9d21c
himanshu raj <himanshu.raj1025@gmail.com>
2023-04-22 22:16:08 +0530

    added static

--- vidyalu/settings.py
+++ vidyalu/settings.py
@@ -163,7 +163,11 @@ USE_TZ = True
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
